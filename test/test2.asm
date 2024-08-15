dc2378eafc1160c4c4f6969182cf4477d6ab7fc3
L.D.C Lin <96625924+DCL-Lawrence@users.noreply.github.com>
2024-08-16 01:40:53 +0800

    Add files via upload

---
+++ test/test2.asm
@@ -0,0 +1,7 @@
+test     start   1000              test program for sic software
+first    lda     one
+         add     three
+         rsub
+one      word    1
+three    word    3
+         end     first
